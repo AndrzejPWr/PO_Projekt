58f19f14be81c315f0223da03f756d095c438a0e
b-romanowicz <64922617+b-romanowicz@users.noreply.github.com>
2020-05-11 16:46:24 +0200

    Delete Krewetka.java

--- test/origin/Krewetka.java
+++
@@ -1,8 +0,0 @@
-package origin;
-
-public class Krewetka extends Organizm {
-
-    public Krewetka(int predkosc, int wiek, int glod, int rozmiar) {
-        super(predkosc, wiek, glod, rozmiar);
-    }
-}
