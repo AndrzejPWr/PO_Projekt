e3ddf83457e9979ba5175a19276b805b5e6f088c
b-romanowicz <64922617+b-romanowicz@users.noreply.github.com>
2020-05-11 16:46:48 +0200

    Delete Slimak.java

--- test/origin/Slimak.java
+++
@@ -1,7 +0,0 @@
-package origin;
-
-public class Slimak extends Organizm {
-    public Slimak(int predkosc, int wiek, int glod, int rozmiar) {
-        super(predkosc, wiek, glod, rozmiar);
-    }
-}
