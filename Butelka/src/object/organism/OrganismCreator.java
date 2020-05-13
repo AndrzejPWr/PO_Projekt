beacd9a2a859593b3cd7e5c99c82ce1b35fbcdf8
jakkard <jakkard@onet.pl>
2020-05-13 17:52:48 +0200

    kreator

--- Butelka/src/object/organism/OrganismCreator.java
+++
@@ -1,5 +0,0 @@
-package object.organism;
-
-public class OrganismCreator {
-
-}
---
+++ Butelka/src/object/organism/creators/OrganismCreator.java
@@ -0,0 +1,29 @@
+package object.organism;
+
+public class OrganismCreator {
+
+	public OrganismCreator() 
+	{
+		
+	}
+
+	public Fish createFish()
+	{
+		
+	}
+	
+	public Fodder createFodder()
+	{
+		
+	}
+	
+	public RapaciousFish createRapaciousFish()
+	{
+		
+	}
+
+	public Snail createSnail()
+	{
+		
+	}
+}
